854a7f625859562a146d1e68d83adc5775913375
Giperbola <giperbola.cpp@gmail.com>
2026-09-07 01:38:17 +0300

    v0.1.0

--- examples/echo/Main.cpp
+++ examples/echo/Main.cpp
@@ -11,19 +11,20 @@ public:
     using TcpSession::TcpSession;
 
 protected:
-    void on_receive(const std::uint8_t* data, std::size_t size) override
-    {
-        send(std::vector<std::uint8_t>(data, data + size));
-    }
-
-    void on_start() override
+    void on_connected() override
     {
         std::cout << "Client connected: " << address() << ':' << port() << std::endl;
     }
 
-    void on_stop() override
+    void on_receive(const std::uint8_t* data, std::size_t size) override
     {
-        std::cout << "Client disconnected" << std::endl;
+        std::cout << "Received " << size << " bytes" << std::endl;
+        send(std::span(data, size));
+    }
+
+    void on_disconnected() override
+    {
+        std::cout << "Client disconnected: " << address() << ':' << port() << std::endl;
     }
 };
 
@@ -42,4 +43,6 @@ int main()
     std::cout << "Netforge server started on port " << server.port() << std::endl;
 
     io.run();
+
+	return 0;
 }
